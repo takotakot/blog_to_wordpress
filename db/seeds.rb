411b943571ccc066922c89d011f4a5dd8d41be78
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-11-21 16:40:10 +0900

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def bulk_insert(classname, data)
+  begin
+    classname.upsert_all(data)
+  rescue ArgumentError => exception
+    data.each do |item|
+      classname.find_or_create_by(item)
+    end
+  end
+end
+bulk_insert(Type, Type::HASH_DATA)
