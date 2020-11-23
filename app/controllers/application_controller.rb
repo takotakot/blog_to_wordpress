dc1d6185ee7dcc31155780de2dfed2140688080e
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-11-23 12:52:56 +0900

    Update method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   INTERVAL = 0.5
 
-  def self.scrape_all_zero
+  def self.scrape_and_analyze_all_zero
     Post.where(status: 0).all.each do |post|
       post.scrape
+      post.analyze
       sleep INTERVAL
     end
   end
