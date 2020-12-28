6ac6219079e569bdd46717b9d8784081ca0a1569
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-12-29 04:23:51 +0900

    WpApiLogHelper

--- app/helpers/wp_api_log_helper.rb
+++ app/helpers/wp_api_log_helper.rb
@@ -1,2 +1,13 @@
 module WpApiLogHelper
+  def self.wp_api_log(wp_id:, method:, query:, endpoint:, ret_json:)
+    log_object = WpApiLog.new
+    log_object.assign_attributes({
+      wp_id: wp_id,
+      method: method,
+      query: query,
+      endpoint: endpoint,
+      ret_json: ret_json,
+    })
+    log_object.save
+  end
 end
