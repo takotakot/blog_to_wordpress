9c2678eb10ae27dad4ce3b547515d29965d4e454
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-12-29 11:12:41 +0900

    Update WordpressPostHelper

--- app/helpers/wordpress_post_helper.rb
+++ app/helpers/wordpress_post_helper.rb
@@ -1,2 +1,18 @@
 module WordpressPostHelper
+  VERSION = 1
+
+  def self.create(post)
+    wp_post = WordpressPost.find_or_initialize_by(post_id: post.id)
+    return nil unless wp_post.new_record?
+
+    # Update attributes
+    wp_post.post_id = post.id
+    wp_post.status = WordpressPost.statuses[:before_processed]
+
+    # For development
+    wp_post.version = VERSION
+
+    return nil unless wp_post.save
+    wp_post
+  end
 end
