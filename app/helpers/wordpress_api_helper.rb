fc4a54cb9f70030bc65914691f1837715b6fa865
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-12-30 17:27:15 +0900

    WordpressApiHelper: Add post_media method

--- app/helpers/wordpress_api_helper.rb
+++ app/helpers/wordpress_api_helper.rb
@@ -49,4 +49,42 @@ module WordpressApiHelper
     end
     return response_body_hash
   end
+
+  def self.post_media(wp_medium, post_data)
+    require 'net/http'
+    require 'net/http/post/multipart'
+
+    uri = self.media_endpoint
+
+    unless wp_medium.wp_id.nil?
+      # update
+      uri += '/' + wp_medium.wp_id.to_s
+      wp_id = wp_medium.wp_id
+    else
+      # new
+    end
+
+    uri_obj= URI.parse(uri)
+    http = Net::HTTP.new(uri_obj.host, uri_obj.port)
+    # http.set_debug_output($stdout)
+
+    req = Net::HTTP::Post::Multipart.new(uri_obj.path, post_data)
+    req.basic_auth(Rails.application.credentials.wp_api_username, Rails.application.credentials.wp_api_password)
+    pp req
+
+    response = http.request(req)
+    response_body_hash = JSON.parse(response.body)
+    wp_id ||= response_body_hash['id']
+
+    p response_body_hash
+    if response.code == '201'
+      p 'Post success: ' + wp_id.to_s
+      WpApiLogHelper.wp_api_log(wp_id: wp_id, method: 'POST', query: post_data, endpoint: uri, ret_json: response.body)
+    else
+      p ['---Failed to WP API Post---', response.code, response.message, response.body].join("\n")
+      WpApiLogHelper.wp_api_log(wp_id: wp_id, method: 'POST', query: post_data, endpoint: uri, ret_json: response.body)
+    end
+    return response_body_hash
+  end
+
 end
