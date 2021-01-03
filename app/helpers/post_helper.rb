c7811305a79a6dc7db39a53331d7661a522a3ee7
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-03 23:07:27 +0900

    Add parepare_wordpress_post

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,2 +1,13 @@
 module PostHelper
+  def self.title(post)
+    post.title
+  end
+
+  def self.content(post)
+    post.article
+  end
+
+  def self.date(post)
+    post.date
+  end
 end
--- app/helpers/wordpress_post_helper.rb
+++ app/helpers/wordpress_post_helper.rb
@@ -15,4 +15,27 @@ module WordpressPostHelper
     return nil unless wp_post.save
     wp_post
   end
+
+  # before_processed -> prepared
+  def self.prepare(posthelper, wp_post)
+    return false unless wp_post.before_processed?
+
+    # Update attributes
+    wp_post.title = posthelper.title(wp_post.post)
+    wp_post.content = posthelper.content(wp_post.post)
+    wp_post.date = posthelper.date(wp_post.post)
+    wp_post.status = WordpressPost.statuses[:prepared]
+
+    # For development
+    wp_post.version = VERSION
+
+    wp_post.save
+  end
+
+  # prepared -> draft_created
+  # draft_created -> categories_set
+  # categories_set -> all_media_uploaded
+  # all_media_uploaded -> rewritten
+  # rewritten -> uploaded
+  # uploaded -> published
 end
