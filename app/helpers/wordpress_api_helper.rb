e986593b8a4d999794ef04a42f8a36df1630c8e1
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-03 23:12:25 +0900

    WordpressApiHelper: Prepare API endpoint

--- app/helpers/wordpress_api_helper.rb
+++ app/helpers/wordpress_api_helper.rb
@@ -1,2 +1,11 @@
 module WordpressApiHelper
+  def self.post_endpoint(wp_post)
+    type_id = wp_post.post.type_id
+    return Rails.application.credentials.wp_api_uri + self.type_id_to_wp_posttype(type_id)
+  end
+
+  # Help determine endpoint
+  def self.type_id_to_wp_posttype(type_id)
+    SecretLogicHelper.type_id_to_wp_posttype(type_id)
+  end
 end
