7f04ee5b25c481f1eb5868f7f0650b80ec203a77
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-04 12:03:33 +0900

    scrape

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,41 @@ class Post < ApplicationRecord
   has_many :tag, through: :post_tag
   has_many :post_medium
   has_many :medium, through: :post_medium
+  # attr_accessor :page
+
+  def scrape
+    set_page
+
+    self.status = get_page_status
+    self.title = get_page_title
+    self.date = get_page_date
+    self.html = get_page_html
+
+    save!
+  end
+
+  def set_page
+    @page = BlogPage::Base.new
+    @page.get(original_uri)
+  end
+
+  def get_page_status
+    @page.status
+  end
+
+  def get_page_html
+    @page.html
+  end
+
+  def get_page_title
+    doc = @page&.doc || set_doc
+    doc.xpath('id("page_title")')[0].text
+  end
+
+  def get_page_date
+    doc = @page&.doc || set_doc
+    doc.xpath('id("page_date")')[0].text
+  end
 
   def set_doc
     @doc ||= Nokogiri::HTML.parse(html)
