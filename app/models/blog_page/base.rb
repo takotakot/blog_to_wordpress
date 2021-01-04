bac4a4ac42e6c95bf109a962ad86d67dd8cd5254
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-04 11:35:02 +0900

    BlogPage: get uri

--- app/models/blog_page/base.rb
+++ app/models/blog_page/base.rb
@@ -1,2 +1,25 @@
-class BlogPage::Base < ApplicationRecord
+class BlogPage::Base
+  # < ApplicationRecord
+  include BlogPage
+  attr_accessor :uri, :charset, :html, :doc, :status
+  require 'open-uri'
+
+  def get(uri)
+    @uri = uri
+    @charset = nil
+    begin
+      @html = URI.open(uri) do |f|
+        @charset = f.charset
+        @status = f.status[0]
+        f.read
+      end
+    rescue OpenURI::HTTPError => e
+      # TODO
+      # raise e
+      @status = e.io.status[0].to_i
+    end
+
+    @doc = Nokogiri::HTML.parse(@html, nil, @charset)
+    self
+  end
 end
