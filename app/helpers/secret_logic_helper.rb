126ea5b32e439f62bde204a143d175cf1a463f58
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-04 16:48:42 +0900

    SecretLogicHelper: post_categories

--- app/helpers/secret_logic_helper.rb
+++ app/helpers/secret_logic_helper.rb
@@ -8,6 +8,27 @@ module SecretLogicHelper
     return posttype
   end
 
+  def self.post_categories(wp_post)
+    type_id = wp_post.post.type_id
+
+    category_name_list = {
+      'cat 1' => [1],
+    }
+
+    category_name = ''
+    category_name_list.each do |k,v|
+      if v.include?(type_id)
+        category_name = k
+        break
+      end
+    end
+    return {} if category_name == ''
+
+    return {
+      category_name => self.post_categories_from_type_id(type_id),
+    }
+  end
+
   def self.post_categories_from_type_id(type_id)
     HASH_DATA.each do |h|
       next unless h[:id] == type_id
