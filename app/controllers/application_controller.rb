6a26020f80e05f94337994f03a94ac6c9acf4379
Takuho NAKANO <takotakot@users.noreply.github.com>
2021-01-04 12:02:18 +0900

    scrape all zero

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  INTERVAL = 0.5
+
+  def self.scrape_all_zero
+    Post.where(status: 0).all.each do |post|
+      post.scrape
+      sleep INTERVAL
+    end
+  end
 end
